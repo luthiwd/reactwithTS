294a60b20e9323417babe388d2e79413b43f32f0
Luthiwd <annattar@gmail.com>
2022-12-15 12:28:30 +0100

    React with TS / practica curso Udemy

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {useState, useEffect} from 'react';
 
+import 'bulma/css/bulma.css'
+import './App.css';
+import UserForm, { UserFormState } from './forms/User';
+
+type User = UserFormState & { id: number }
 function App() {
+  const [users,setUsers] = useState<User[]>([])
+
+
+  async function fetchUsers() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data: User[] = await response.json()
+    setUsers(data)
+  }
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+      <UserForm handleSubmit={user => setUsers([...users, {...user, id: Date.now()}])}/>
     </div>
   );
 }
